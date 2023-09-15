22c16cb425b52711f2d617ecc9025750a2e98f50
SNVCRAZY <2557918392@qq.com>
2023-09-15 18:44:59 +0800

    1

--- vue3+ts/vue3-ts-demo/src/views/LoginView.vue
+++ vue3+ts/vue3-ts-demo/src/views/LoginView.vue
@@ -1,25 +1,90 @@
 <template>
     <div class="login-box">
-        登陆页面
+        <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" label-width="80px" class="demo-ruleForm">
+            <h2>后台管理系统</h2>
+            <el-form-item label="账号：" prop="username">
+                <el-input v-model="ruleForm.username" autocomplete="off" />
+            </el-form-item>
+
+            <el-form-item label="密码：" prop="password">
+                <el-input v-model="ruleForm.password" type="password" autocomplete="off" />
+            </el-form-item>
+            <el-form-item>
+                <el-button class="loginBtn" type="primary" @click="submitForm(ruleFormRef)">登录</el-button>
+                <el-button class="loginBtn" @click="resetForm">重置</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, toRefs } from 'vue'
+
 
 export default defineComponent({
-    setup () {
-        
+    setup() {
+        const data = reactive({
+            ruleForm: {
+                username: "",
+                password: ""
+            }
+        }),
+            rules: {
+                username: [
+                    {
+                        required: true,
+                        message: '请输入账号',
+                        trigger: 'blur'
+                    },
+                    {
+                        min: 6,
+                        max: 10,
+                        message: '账号应为6-10位',
+                        trigger: 'blur'
+                    },
+                ],
+                password: [
+                    {
+                        required: true,
+                        message: '请输入密码',
+                        trigger: 'blur'
+                    },
+                    {
+                        min: 6,
+                        max: 10,
+                        message: '密码应为6-10位',
+                        trigger: 'blur'
+                    },
+                ],
+            }
 
-        return {}
+        return { ...toRefs(data) }
     }
 })
 </script>
 
 <style lang="scss" scoped>
-    .login-box{
-        width: 100%;
-        height: 100%;
-        background: url(../assets/bg.jpg);
+.login-box {
+    width: 100%;
+    height: 100%;
+    background: url("../assets/bg.jpg");
+    padding: 1px;
+    text-align: center;
+
+    .demo-ruleForm {
+        width: 500px;
+        margin: 200px auto;
+        background-color: #fff;
+        padding: 40px;
+        border-radius: 20px;
     }
+
+    .loginBtn {
+        width: 48%;
+    }
+
+    h2 {
+        margin-bottom: 10px;
+    }
+}
 </style>
